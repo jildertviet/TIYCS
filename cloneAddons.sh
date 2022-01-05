55312586c07d31961a952b0228e5a600124b5197
Jildert Viet <jivido@hotmail.com>
2022-01-05 17:23:49 +0100

    Script for cloning addons

--- cloneAddons.sh
+++ cloneAddons.sh
@@ -1,3 +1,5 @@
 git clone https://github.com/arturoc/ofxEasing.git
 git clone https://github.com/elgiano/ofxOscBidirectional.git
 git clone https://github.com/jildertviet/ofxJVisuals.git
+git clone https://github.com/vanderlin/ofxBox2d.git
+git clone https://github.com/jvcleave/ofxOMXPlayer.git
